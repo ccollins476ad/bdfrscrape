ef9355d0b361078eeb8d19a9b5c60a93ba394f3d
Christopher Collins <ccollins@apache.org>
2026-06-06 16:16:37 -0700

    Allow single file source
    Allow the source argument to specify a single BDFR json file, rather
    than a subreddit direectory. This is intended for debugging only.

--- main.go
+++ main.go
@@ -29,24 +29,37 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	// Collect filenames of posts in source directory.
+	// Collect filenames of posts to process.
 	var filenames []string
-	filepath.WalkDir(cfg.Source, func(path string, d fs.DirEntry, err error) error {
-		if !d.IsDir() && strings.HasSuffix(d.Name(), ".json") {
-			filenames = append(filenames, d.Name())
-		}
-		return nil
-	})
 
-	// Copy non-post files to destination directory. These are files that won't
-	// get processed, but which the posts reference, and thus need a manual
-	// copy (reddit media).
-	err = fileutil.RecursiveCopyIf(cfg.Source, cfg.DestDir, func(info os.FileInfo) bool {
-		return info.IsDir() || !strings.HasSuffix(info.Name(), ".json")
-	})
+	fi, err := os.Stat(cfg.Source)
 	if err != nil {
 		printFatalError(err)
-		os.Exit(2)
+		os.Exit(3)
+	}
+
+	if !fi.IsDir() {
+		// User specified a single file.
+		filenames = []string{filepath.Base(cfg.Source)}
+		cfg.Source = filepath.Dir(cfg.Source)
+	} else {
+		// User specified a directory. Recursively collect all json files.
+		filepath.WalkDir(cfg.Source, func(path string, d fs.DirEntry, err error) error {
+			if !d.IsDir() && strings.HasSuffix(d.Name(), ".json") {
+				filenames = append(filenames, d.Name())
+			}
+			return nil
+		})
+		// Copy non-post files to destination directory. These are files that won't
+		// get processed, but which the posts reference, and thus need a manual
+		// copy (reddit media).
+		err = fileutil.RecursiveCopyIf(cfg.Source, cfg.DestDir, func(info os.FileInfo) bool {
+			return info.IsDir() || !strings.HasSuffix(info.Name(), ".json")
+		})
+		if err != nil {
+			printFatalError(err)
+			os.Exit(2)
+		}
 	}
 
 	processFiles(context.Background(), cfg, filenames)
